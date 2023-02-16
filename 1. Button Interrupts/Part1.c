5c26e06f0bd73d928a24782b77dd7c8dfe574e4a
Joseff Santiago <santia76@students.rowan.edu>
2023-02-16 16:19:19 -0500

    Full functionality for part 1

--- 1. Button Interrupts/Part1.c
+++ 1. Button Interrupts/Part1.c
@@ -31,7 +31,7 @@ int main(void)
 
     while(1)
     {
-        if (LED_Color) P1OUT ^= BIT0;                  // P1.0 = toggle
+        if (LED_Color) P1OUT ^= BIT0;       // P1.0 = toggle
         else P6OUT ^= BIT6;                 // P6.6 = toggle
         __delay_cycles(100000);
     }
@@ -51,7 +51,7 @@ void gpioInit(){
       P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
       P2REN |= BIT3;                          // P2.3 pull-up register enable
       P2IES &= ~BIT3;                         // P2.3 Low --> High edge
-      P2IE |= BIT3;                           // P2.3 interrupt enabled
+      P2IE  |= BIT3;                          // P2.3 interrupt enabled
 }
 
 
@@ -59,17 +59,24 @@ void gpioInit(){
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
-    P2IFG &= ~BIT3;                         // Clear P1.3 IFG
+    P2IFG &= ~BIT3; // Clear P1.3 IFG
 
-    if ( )       // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a rising edge.
+    if (P2IES & BIT3)       // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a rising edge.
     {
         LED_Color = 0;
+        P2IES &= ~BIT3;
+        P6OUT |= BIT6;
+
         // @TODO Add code to change which edge the interrupt should be looking for next
     }
 
-    else if ( ) // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a falling edge.
+    else if (!(P2IES&BIT3)) // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a falling edge.
     {
         LED_Color = 1;
+        P2IES |= BIT3;
+        P6OUT &= ~BIT6;
+
         // @TODO Add code to change which edge the interrupt should be looking for next
     }
+    P2IE |=BIT3;
 }
